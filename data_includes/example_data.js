22b60f88097487e56a22b309734b51369c3f9f93
PennController <39499242+PennController@users.noreply.github.com>
2018-09-13 23:24:38 -0400

    Priming Feed

--- data_includes/example_data.js
+++ data_includes/example_data.js
@@ -1,35 +1,42 @@
-var shuffleSequence = seq("trial");
+// Show the picture trials first (though we generate them second)
+var shuffleSequence = seq(randomize("picture"), randomize("rating"));
 PennController.ResetPrefix(null);
+// We don't give the full URL in the spreadsheet
 PennController.AddHost("http://files.lab.florianschwarz.net/ibexfiles/PennController/SampleTrials/");
 
-var items = [
-    // Picture selection trial
-    ["trial", "PennController", PennController(
+// As before, we use FeedItems to define a template
+// But now, we also specify that we want to use a subset of the table:
+// this template only uses the rows where Label is 'rating'
+PennController.FeedItems( PennController.defaultTable.filter("Label","rating") ,
+    row => PennController(
+        newText( "green" , row.Sentence )
+        ,
+        newScale("judgment",    "cold", "cool", "lukewarm", "warm", "hot")
+            .settings.labels("top")
+            .settings.before( getText("green") )
+            .settings.size("auto")
+            .print()
+            .wait()
+    )
+);
+
+// We use FeedItems a second time to define a template for the 'picture' trials
+// This template only uses the rows where Label is 'picture'
+PennController.FeedItems( PennController.defaultTable.filter("Label","picture") ,
+    row => PennController(
         defaultImage
             .settings.size(200, 200)
         ,
-        newText("test sentence", "Which patch is greener?")
+        newText("test sentence", row.Sentence)
             .print()
         ,
         newCanvas("patches", 500, 200)
-            .settings.add(   0, 0, newImage("color1", "green1.png") )   // Embedded image creation
-            .settings.add( 300, 0, newImage("color2", "green2.png") )   // Embedded image creation
+            .settings.add(   0, 0, newImage("color1", row.Color1) )
+            .settings.add( 300, 0, newImage("color2", row.Color2) )
             .print()
         ,
         newSelector("patch")
             .settings.add( getImage("color1") , getImage("color2") )
             .wait()
-    )]
-    ,
-    // Rating trial
-    ["trial", "PennController", PennController(
-        newText("green", "To me the color green is...")
-        ,
-        newScale("judgment",    "cold", "cool", "lukewarm", "warm", "hot")
-            .settings.labels("top")
-            .settings.before( getText("green") )
-            .settings.size("auto")  // Use max width for each cell (here, lukewarm's cell's width)
-            .print()
-            .wait()
-    )]
-];
+    )
+);
